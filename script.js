3b3e73352b3b5b11126420e82977b8b133c2eafb
nico spinelli <nicospinelli88@gmail.com>
2024-08-22 16:50:27 -0300

    calculadora proyecto

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ buttons.forEach((btn) => {
         if(btn.id === "="){
             display.value = eval(display.value);
         } else if (btn.id === "ac") {
-            display.value = "";
+            display.value = "0";
         } else if (btn.id == "de") {
             display.value = display.value.slice(0, -1);
         } else{
